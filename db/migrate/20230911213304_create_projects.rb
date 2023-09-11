95f7e27ff4905c0c594f4ac3ef9df409bb538a35
21mad <ilmir.abyzov@mail.ru>
2023-09-12 00:39:18 +0300

    Migrated + root change

--- db/migrate/20230911213304_create_projects.rb
+++ db/migrate/20230911213304_create_projects.rb
@@ -1,11 +1,11 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-      t.string :name
+      t.string :name, null: false
       t.text :description
-      t.date :deadline
+      t.date :deadline, null: false
       t.date :end_date
-      t.boolean :active
+      t.boolean :active, default: true, null: false
 
       t.timestamps
     end
